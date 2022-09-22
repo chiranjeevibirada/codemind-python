83c7a5a5e491659ad25d6156178ebc8204c80574
chiranjeevibirada <109719934+chiranjeevibirada@users.noreply.github.com>
2022-09-22 15:21:31 +0530

    n = int(input()) i=1 c=0 while i<=n:         if i*i ==n:             print("True")             c=1             break         i+=1 if c==0:     print("False")

--- perfect_square_root_or_not.py
+++ perfect_square_root_or_not.py
@@ -2,11 +2,10 @@ n = int(input())
 i=1
 c=0
 while i<=n:
-    if (i*i)==n:
-        print("True")
-        c=1
-        break
-    i+=1
+        if i*i ==n:
+            print("True")
+            c=1
+            break
+        i+=1
 if c==0:
     print("False")
-    
