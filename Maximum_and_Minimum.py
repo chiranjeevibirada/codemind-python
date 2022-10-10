31f789d268777aaac04eed2890127884b5694d3a
chiranjeevibirada <109719934+chiranjeevibirada@users.noreply.github.com>
2022-10-10 16:16:56 +0530

    n = int(input()) l = list(map(int,input().split())) x=[] for i in l:     if l.count(i)==i:         if i not in x:             x.append(i) if len(x)>0:         print(min(x),max(x)) else:     print(-1)

---
+++ Maximum_and_Minimum.py
@@ -0,0 +1,11 @@
+n = int(input())
+l = list(map(int,input().split()))
+x=[]
+for i in l:
+    if l.count(i)==i:
+        if i not in x:
+            x.append(i)
+if len(x)>0:
+        print(min(x),max(x))
+else:
+    print(-1)
