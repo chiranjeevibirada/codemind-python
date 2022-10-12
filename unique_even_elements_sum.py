6a6a1edde42e4b1daa1a6376bb5aa59e4017a4fb
chiranjeevibirada <109719934+chiranjeevibirada@users.noreply.github.com>
2022-10-12 11:55:30 +0530

    n = int(input()) lst = list(map(int, input().split())) c=0 x = [] for i in lst:     if i%2==0 and i not in x:         x.append(i) for i in x:     c+=i print(c)

--- unique_even_elements_sum.py
+++ unique_even_elements_sum.py
@@ -1,10 +1,10 @@
 n = int(input())
 lst = list(map(int, input().split()))
+c=0
 x = []
-s=0
 for i in lst:
     if i%2==0 and i not in x:
         x.append(i)
 for i in x:
-    s+=i
-print(s)
+    c+=i
+print(c)
