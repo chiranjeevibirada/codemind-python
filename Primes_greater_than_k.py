ddd79b1550aa086474cddb71f75290a28ed74964
chiranjeevibirada <109719934+chiranjeevibirada@users.noreply.github.com>
2022-10-14 11:17:31 +0530

    def is_prime(x):     fc=0     for i in range(1,x+1):         if x%i==0:             fc+=1     if fc==2:         return True     return False n = int(input()) lst=list(map(int,input().split())) k = int(input()) c=0 for i in lst:     if i>=k:         if is_prime(i)==True:             c+=1 print(c)

---
+++ Primes_greater_than_k.py
@@ -0,0 +1,17 @@
+def is_prime(x):
+    fc=0
+    for i in range(1,x+1):
+        if x%i==0:
+            fc+=1
+    if fc==2:
+        return True
+    return False
+n = int(input())
+lst=list(map(int,input().split()))
+k = int(input())
+c=0
+for i in lst:
+    if i>=k:
+        if is_prime(i)==True:
+            c+=1
+print(c)
