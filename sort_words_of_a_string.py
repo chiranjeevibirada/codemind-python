23f8ca2075ad383b56c4ab2c3a8e48bd762beb91
chiranjeevibirada <109719934+chiranjeevibirada@users.noreply.github.com>
2022-10-28 15:47:58 +0530

    s= input().split() for i in s:     v = []     l = []     z = ""     j,k=0,0     for m in i:         if (m >="a" and m<="z")or(m>="A" and m<="Z"):             l.append(m)         else:             v.append(m)     l=sorted(l)     for p in i:         if p in l:             z=z+l[j]             j+=1         else:             z=z+v[k]             k+=1     print(z, end = ' ')

---
+++ sort_words_of_a_string.py
@@ -0,0 +1,20 @@
+s= input().split()
+for i in s:
+    v = []
+    l = []
+    z = ""
+    j,k=0,0
+    for m in i:
+        if (m >="a" and m<="z")or(m>="A" and m<="Z"):
+            l.append(m)
+        else:
+            v.append(m)
+    l=sorted(l) 
+    for p in i:
+        if p in l:
+            z=z+l[j]
+            j+=1
+        else:
+            z=z+v[k]
+            k+=1
+    print(z, end = ' ')
