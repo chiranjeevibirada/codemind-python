8dac6b29fdd7be617502cab17ec693c4d63350a3
chiranjeevibirada <109719934+chiranjeevibirada@users.noreply.github.com>
2022-10-31 16:31:55 +0530

    s1=input().lower() s2=input().lower() s1=s1.split() s2=s2.split() x=[] c=0 if len(s2)>len(s1):     s1,s2=s2,s1 for i in s1:     if i.isalpha() and i in s2 and i not in x:         x.append(i)         c+=1 for i in s2:     if i.isalpha() and i in s1 and i not in x:         x.append(i)         c+=1 print(c)

--- common_words_-1.py
+++ common_words_-1.py
@@ -7,11 +7,11 @@ c=0
 if len(s2)>len(s1):
     s1,s2=s2,s1
 for i in s1:
-    if i in s2 and i not in x:
+    if i.isalpha() and i in s2 and i not in x:
         x.append(i)
         c+=1
 for i in s2:
-    if i in s1 and i not in x:
+    if i.isalpha() and i in s1 and i not in x:
         x.append(i)
         c+=1
 print(c)
